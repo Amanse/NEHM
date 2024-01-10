c5fc53c07bf6461f67a55eab6db91cfdfc02a542
AmanSe <amansetiarjp@gmail.com>
2024-01-10 22:21:37 +0530

    Move notes to mongodb

--- handlers/notes.js
+++ handlers/notes.js
@@ -1,24 +1,17 @@
-import db from "../lib/db.js";
 import showdown from "showdown";
 import sanitizeHtml from "sanitize-html";
 import { getUserInfoFromToken } from "./auth.js";
+import Note from "../models/notes.js";
 
 export const getAllNotes = async (req, res) => {
-  const d = await db;
-
   var { id } = getUserInfoFromToken(req.cookies.auth);
 
-  const re = await d.all(
-    "SELECT id, body FROM notes where uid=? order by created_at desc",
-    id,
-  );
+  const re = await Note.find({ uid: id }).sort({ _id: -1 });
 
   res.render("notes/notes", { notes: re });
 };
 
 export const addNote = async (req, res) => {
-  const d = await db;
-
   const { body } = req.body;
 
   const { id } = getUserInfoFromToken(req.cookies.auth);
@@ -27,32 +20,28 @@ export const addNote = async (req, res) => {
   var tb = convertor.makeHtml(body);
 
   const clean = sanitizeHtml(tb);
-  const r = await d.run(
-    "Insert into notes (uid, body) values (?,?)",
-    id,
-    clean,
-  );
+  const newNote = Note({
+    uid: id,
+    body: clean,
+  });
 
-  res.render("notes/note", { body: clean, id: r.lastID });
+  const r = await newNote.save();
+
+  res.render("notes/note", { body: clean, id: r._id });
 };
 
 export const deleteNote = async (req, res) => {
-  const d = await db;
-
-  // console.log(req.body);
-  // const { id } = req.body;
-  // console.log(id);
   const { id } = req.params;
 
   const a = getUserInfoFromToken(req.cookies.auth);
 
-  const re = await d.get("SELECT uid from notes where id = ?", id);
+  const re = await Note.findById(id);
 
   if (re.uid != a.id) {
     res.status(401).send("Cannot delete someone else's note");
     return;
   }
 
-  await d.run("delete from notes where id = ?", id);
+  await Note.deleteOne(re);
   res.send("success");
 };
